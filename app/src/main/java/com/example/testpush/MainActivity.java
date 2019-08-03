b351128bfec25b9efd894de738ac65eda80bc875
Nishiki97 <nsihikiyapa1997@gmail.com>
2019-08-03 11:39:41 +0530

    Modified MainActivity added new file TestAddFile.java

--- app/src/main/java/com/example/testpush/MainActivity.java
+++ app/src/main/java/com/example/testpush/MainActivity.java
@@ -15,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private void test(){
 
         int i = 1 + 1;
+
+        int ii = 2 + 2;
+
+        int iii = 3 + 3;
     }
 }
---
+++ app/src/main/java/com/example/testpush/TestAddFile.java
@@ -0,0 +1,4 @@
+package com.example.testpush;
+
+public class TestAddFile {
+}
